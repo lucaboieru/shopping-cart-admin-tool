51d104ca6a75670686cbbfba080c043c112e011b
Dan Andrei <andrei_dani_96@yahoo.com>
2014-05-30 23:36:04 +0300

    added log support

--- modules/management/operations.js
+++ modules/management/operations.js
@@ -1,41 +1,132 @@
 var fs = require('fs');
 var git = require('gift');
 var exec = require('child_process').exec;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 
 exports.restart = function (link) {
 
-	var app = "shopping-cart-server";
+    var app = "shopping-cart-server";
 
-	exec("forever stop apps/" + app + "/server.js", function (err) {
+    exec("forever stop apps/" + app + "/server.js", function (err) {
 
-		if (err) { console.log(err); }
+        if (err) { console.log(err); }
 
-		exec("rm -Rf apps/" + app, function (err) {
+        exec("rm -Rf apps/" + app, function (err) {
 
-			if (err) { console.log(err); }
+            if (err) { console.log(err); }
 
-			git.clone("git@bitbucket.org:lucaboieru/" + app + ".git", "apps/" + app, function (err, repo) {
-				if (err) { console.log(err); }
+            git.clone("git@bitbucket.org:lucaboieru/" + app + ".git", "apps/" + app, function (err, repo) {
+                if (err) { console.log(err); }
 
-				exec("forever start apps/" + app + "/server.js", function (err) {
+                exec("forever start apps/" + app + "/server.js", function (err) {
 
-					if (err) { console.log(err); }
+                    if (err) { console.log(err); }
 
-					link.res.writeHead(200, {'Content-Type': 'text/plain'});
-					link.res.end("App updated!");
-				});
-			});
-		});
-	});
+                    link.res.writeHead(200, {'Content-Type': 'text/plain'});
+                    link.res.end("App updated!");
+                });
+            });
+        });
+    });
 }
 
-exports.log = function (link) {
+exports.getLogs = function (link) {
 
-	var app = "shopping-cart-server";
+    if (!link.data.log) {
+        link.res.end();
+        return;
+    }
 
-	fs.readFile("logs/" + app + ".log", "utf8", function (err, data) {
-		if (err) { console.err(err); }
-		link.res.writeHead(200, {'Content-Type': 'text/plain'});
-		link.res.end(data);
-	});
+    var log = link.data.log;
+
+    // connect to db
+    MongoClient.connect('mongodb://127.0.0.1:27017/scanner', function (err, db) {
+        
+        if (err) {
+            link.res.writeHead(500);
+            link.res.end();
+            return;
+        }
+
+        // get collection
+        var col = db.collection('logs');
+
+        // get the logs
+        col.find({ 'log': log }).toArray(function (err, docs) {
+
+            if (err) {
+                link.res.writeHead(500);
+                link.res.end();
+                return;
+            }
+            // send response
+            link.res.setHeader('Access-Control-Allow-Origin', link.headers.origin);
+            link.res.end(JSON.stringify(docs));
+
+            // close the connection
+            db.close();
+        });
+    });
+}
+
+exports.getLog = function (link) {
+    
+    // set the response header
+    link.res.setHeader('Access-Control-Allow-Origin', link.headers.origin);
+
+    if (!link.data.id) {
+        link.res.writeHead(500);
+        link.res.end();
+        return;
+    }
+
+    var itemId = link.data.id;
+
+    // connect to db
+    MongoClient.connect('mongodb://127.0.0.1:27017/scanner', function (err, db) {
+        
+        if (err) {
+            link.res.writeHead(500);
+            link.res.end();
+            return;
+        }
+
+        // get collection
+        var col = db.collection('logs');
+
+        // get the logs
+        col.findOne({ '_id': ObjectId(itemId) }, function (err, doc) {
+
+            // handle error
+            if (err) {
+                link.res.writeHead(500);
+                link.res.end();
+                return;
+            }
+            if (!doc) {
+                console.error('log not found');
+                link.res.writeHead(500);
+                link.res.end();
+                return;
+            }
+
+            // get the file location
+            var file = doc.location;
+
+            // read the file
+            fs.readFile(file, 'utf8', function (err, data) {
+
+                if (err) {
+                    console.log(err);
+                    link.res.writeHead(500);
+                    link.res.end();
+                    return;
+                }
+
+                // send back the contents of the log
+                link.res.end(JSON.stringify(data));
+            });
+        });
+    });
 }
---
+++ public/js/functions.js
@@ -0,0 +1,127 @@
+var URL = 'http://127.0.0.1:9016'
+var ACTIVE_TAB;
+
+$(document).ready(function () {
+
+    // navigation
+    selectItem($('.tabSel').first());
+    $('.tabSel').on('click', function () {
+        selectItem($(this));
+    });
+
+    // clear log
+    $('.clearBtn').on('click', function () {
+        $('.logContent').html('').addClass('hided');
+    });
+});
+
+function selectItem ($item) {
+
+    // hide all tabs and active class
+    $('.tab').addClass('hided');
+    $('.tabSel').parent().removeClass('active');
+
+    // select the item
+    $('#' + $item.attr('data-action')).removeClass('hided');
+    ACTIVE_TAB = $('#' + $item.attr('data-action'));
+    $('.logContent').addClass('hided');
+    $item.parent().addClass('active');
+
+
+    // get the logs for the selected item
+    getLogs($item.attr('log'));
+}
+
+function getLogs (item) {
+
+    if (!item) { return; }
+
+    // make the request for the logs
+    makeAjaxRequest({
+        operation: '/@/management/getLogs',
+        data: {
+            log: item
+        }
+    }, function (err, data) {
+        
+        if (err) { return; }
+
+        // get the data object
+        var docs = jQuery.parseJSON(data);
+
+        if (!docs.length) { return; }
+
+        var $list = $('.log[log=' + item + '] ul');
+        $list.html('');
+
+        // append the files
+        for (var i in docs) {
+            var el = '<li class="file mb-xs" data-id="' + docs[i]._id + '">';
+            el += '<div class="file-group">' + docs[i].filename + '</div>';
+            el += '</li>';
+
+            $list.append(el);
+        }
+
+        // add click handler
+        $('.file', $list).on('click', function () {
+
+            var itemId = $(this).attr('data-id');
+            
+            // make the request for the log
+            makeAjaxRequest({
+                operation: '/@/management/getLog',
+                data: {
+                    id: itemId
+                }
+            }, function (err, data) {
+                
+                if (err) {
+                    $('.logContent', ACTIVE_TAB).html(err).removeClass('hided');
+                    return;
+                }
+                showLog(data);
+            });
+        });
+    });
+}
+
+function showLog (data) {
+
+    if (!data) { return; }
+
+    data = jQuery.parseJSON(data).toString();
+
+    // add the data to the container
+    $('.logContent', ACTIVE_TAB).html(data).removeClass('hided');
+}
+
+function makeAjaxRequest (ajaxObj, callback) {
+
+    $.ajax({
+        url: URL + ajaxObj.operation,
+        type: "POST",
+        data: ajaxObj.data,
+        crossDomain: true,
+        error: function(jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                callback('Not connect. Verify Network.');
+            } else if (jqXHR.status == 404) {
+                callback('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                callback('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                callback('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                callback('Time out error.');
+            } else if (exception === 'abort') {
+                callback('Ajax request aborted.');
+            } else {
+                callback('Uncaught Error.\n' + jqXHR.responseText);
+            }
+        },
+        success: function (data) {
+            callback(null, data);
+        }
+    });
+}
